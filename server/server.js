b962214e44bff8903676ab9aff5eab170cb318e4
inzeela0111 <inzeela0111@gmail.com>
2026-06-02 14:40:51 +0530

    Fix wildcard route for Express 5 compatibility

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ app.use("/api/credits/request" , creditRequestRoutes)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../Client/dist')))
 
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.resolve(__dirname, '../Client', 'dist', 'index.html'))
     })
 }
